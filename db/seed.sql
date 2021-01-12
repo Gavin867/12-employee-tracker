bfa1b4ec2fdb6aab0d2633f0f3d144fd0ea95cb3
Gavin Calkins <gavin.calkins@gmail.com>
2021-01-12 14:13:49 -0800

    data base structure and see file code added

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (name)
+VALUES ("Silly Walks");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Minister of Silly Walks", 50000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Cleese", 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Eric", "Palin", 1, 1);
