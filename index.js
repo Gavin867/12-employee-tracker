7922e1aa620c6d29781b94d6a0a36ee1b30aafd4
Gavin Calkins <gavin.calkins@gmail.com>
2021-01-12 13:28:34 -0800

    initial file structure and code

---
+++ index.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+
+require("dotenv").config(); 
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: process.env.DB_USER,
+
+  // Your password
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  init();
+});
+
+function init() {
+
+    console.log("welcome to employee tracker");
+ 
+}
