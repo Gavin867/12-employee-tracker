ff4edbd162a7a880a37dd8bd12a8269be408b430
Gavin Calkins <gavin.calkins@gmail.com>
2021-01-22 16:17:22 -0800

    working on function files in database

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-const mysql = require("mysql");
+const connection = require("./db/functions/connection");
 
-const util = require("util");
+const deparments = require("./db/functions/departments");
+
+// Roles, Employees
 
 const inquirer = require("inquirer");
-
-require("dotenv").config();
-
-const connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: process.env.DB_USER,
-
-  // Your password
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  init();
-});
 
 function init() {
 
@@ -60,7 +40,8 @@ function init() {
       switch (userResponse.action) {
 
         case "VIEW Departments":
-          // code block
+          
+          departments.viewAll();
 
           break;
 
@@ -119,6 +100,6 @@ function init() {
       }
 
     })
+};
 
-
-}
+init();
---
+++ db/functions/departments.js
@@ -0,0 +1,36 @@
+const connection = require("./db/functions/connection");
+
+class Department {
+
+    constructor(connection) {
+        this.connection = connection;
+    };
+
+    // View all deparments
+
+    viewAll() {
+
+        console.log("Viewing All Departments...");
+
+        return this.connection.query("select * from department", function (err, result) {
+
+            if (err) {
+
+                console.log(err);
+
+            } else {
+
+                console.table(result);
+
+                init();
+            }
+        });
+    }
+
+    // Add departments
+
+    // Delete Departments
+
+};
+
+module.exports = new Department(connection); 
