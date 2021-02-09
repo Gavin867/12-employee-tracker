1cb6a2c570609eb789f2a3360327e112a2c479ed
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-08 17:16:36 -0800

    seed file corrected and runs now

--- db/mysql/employeeDB.sql
+++ db/mysql/employeeDB.sql
@@ -5,27 +5,27 @@ CREATE DATABASE employeeDB;
 USE employeeDB;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
+  department_id INT NOT NULL AUTO_INCREMENT,
   department_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (department_id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
+  role_id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
-  FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE,
-  PRIMARY KEY (id)
+  FOREIGN KEY (department_id) REFERENCES department (department_id) ON DELETE CASCADE,
+  PRIMARY KEY (role_id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+  employee_id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NULL,
-  FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE,
-  FOREIGN KEY (manager_id ) REFERENCES employee (id ) ON DELETE SET NULL,
-  PRIMARY KEY (id)
+  FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id ) REFERENCES employee (employee_id) ON DELETE SET NULL,
+  PRIMARY KEY (employee_id)
 );
--- db/mysql/employeeDB_seeds.sql
+++ db/mysql/employeeDB_seeds.sql
@@ -14,7 +14,8 @@ VALUES
   ("Executive Silly Assistant", 35000, 1),
   ("Norwegian Blue Expert", 25000, 2),
   ("Plumage Analyst", 20000, 2),
-  ("Head Inquisitor", 60000, 3) ("Comfy Chair Inquisitor", 40000, 3),
+  ("Head Inquisitor", 60000, 3),
+  ("Comfy Chair Inquisitor", 40000, 3),
   ("Soft Pillow Operator", 20000, 3),
   ("Arguer", 40000, 4),
   ("Front Desk", 30000, 4),
@@ -23,15 +24,15 @@ VALUES
 INSERT INTO
   employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ("Richard", "Teabag", 1, 1),
-  ("Mel", "Two-Lumps", 1, NULL),
+  ("Richard", "Teacup", 1, 1),
+  ("Sandy", "Beech", 1, NULL),
   ("Joe", "King", 2, 2),
-  ("Charles", "Praline", 2, Null),
-  ("Cardinal, Ximinez", 3, 3),
+  ("Charles", "Praline", 2, NULL),
+  ("Cardinal", "Ximinez", 3, 3),
   ("Cardinal", "Biggles", 3, NULL),
   ("Cardinal", "Fang", 3, NULL),
   ("John", "Barnard", 4, 4),
   ("Wanda", "Fish", 4, NULL),
-  ("Chacy", "Beck", 5, 5);
+  ("Tracy", "Beck", 5, 5);
 
 -- DELETE FROM employee WHERE id=2;
