2ab79975f6cdd4ed8063ef9c1197ce14841d8e02
Gavin Calkins <gavin.calkins@gmail.com>
2021-02-11 19:09:48 -0800

    working on add employee function

--- app.js
+++ app.js
@@ -2,81 +2,81 @@ const connection = require("./database/connection");
 const inquirer = require("inquirer");
 const database = require("./database");
 const consoletable = require("console.table");
-const { addEmployee } = require("./database");
+// const { addEmployee } = require("./database");
 
 function init() {
   console.log("Welcome to the Employee Tracker!");
-  inquirer
-    .prompt({
-      type: "list",
-      name: "action",
-      message: "Select an action.",
-      choices: [
-        "VIEW Departments",
-        "VIEW Roles",
-        "VIEW Employees",
-        "ADD Department",
-        "ADD Role",
-        "ADD Employee",
-        "UPDATE Role",
-        "DELETE Department",
-        "DELETE Role",
-        "DELETE Employee",
-        "EXIT Application"
-      ]
-    }).then(userResponse => {
-      switch (userResponse.action) {
-        case "VIEW Departments":
-          viewDepartments();
+  inquirer.prompt({
+    type: "list",
+    name: "action",
+    message: "Select an action.",
+    choices: [
+      "VIEW Departments",
+      "VIEW Roles",
+      "VIEW Employees",
+      "ADD Department",
+      "ADD Role",
+      "ADD Employee",
+      "UPDATE Role",
+      "DELETE Department",
+      "DELETE Role",
+      "DELETE Employee",
+      "EXIT Application"
+    ]
+  }).then(userResponse => {
+    switch (userResponse.action) {
+      case "VIEW Departments":
+        viewDepartments();
 
-          break;
+        break;
 
-        case "VIEW Roles":
-          viewRoles();
+      case "VIEW Roles":
+        viewRoles();
 
-          break;
+        break;
 
-        case "VIEW Employees":
-          viewEmployees();
+      case "VIEW Employees":
+        viewEmployees();
 
-          break;
+        break;
 
-        case "ADD Department":
-          addDepartment();
+      case "ADD Department":
+        addDepartment();
 
-          break;
+        break;
 
-        case "ADD Role":
-          addRole();
+      case "ADD Role":
+        addRole();
 
-          break;
+        break;
 
-        case "ADD Employee":
+      case "ADD Employee":
+        addEmployee();
 
-          break;
+        break;
 
-        case "UPDATE Role":
+      case "UPDATE Role":
 
-          break;
+        break;
 
-        case "DELETE Department":
+      case "DELETE Department":
 
-          break;
+        break;
 
-        case "DELETE Role":
+      case "DELETE Role":
 
-          break;
+        break;
 
-        case "DELETE Employee":
+      case "DELETE Employee":
 
-          break;
+        break;
 
-        case "EXIT Application":
-          connection.end();
+      case "EXIT Application":
+        connection.end();
 
-          break;
-      }
-    })
+        break;
+    }
+  })
 };
 
 function viewDepartments() {
@@ -101,55 +101,74 @@ function viewEmployees() {
 };
 
 function addDepartment() {
-  inquirer
-    .prompt(
-      {
-        type: "input",
-        name: "departmentName",
-        message: "What is the name of your new department?",
-      }).then(newDepartment => {
-        database.createDepartment(newDepartment).then(result => {
-          console.log("Your new department has been added.");
-          init();
-        });
+  inquirer.prompt(
+    {
+      type: "input",
+      name: "departmentName",
+      message: "What is the name of your new department?",
+    }).then(newDepartment => {
+      database.createDepartment(newDepartment).then(result => {
+        console.log("Your new department has been added.");
+        init();
       });
+    });
 };
 
 function addRole() {
   database.readDepartments().then(departments => {
-
     let departmentOptions = departments.map(departments => ({
       value: departments.department_id,
       name: departments.department_name
     }));
 
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "roleTitle",
-          message: "What is the title of this new role?"
-        },
-        {
-          type: "input",
-          name: "roleSalary",
-          message: "In dollars, what is the compensation associated with this role?"
-        },
-        {
-          type: "list",
-          name: "departmentId",
-          message: "To which department does this role belong?",
-          choices: departmentOptions
-        },
-      ]).then(newRoleInfo => {
-        database.createRole(newRoleInfo).then(result => {
-          console.table(result);
-          init();
-        });
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "roleTitle",
+        message: "What is the title of this new role?"
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "In dollars, what is the yearly compensation associated with this role?"
+      },
+      {
+        type: "list",
+        name: "departmentId",
+        message: "To which department does this role belong?",
+        choices: departmentOptions
+      },
+    ]).then(newRoleInfo => {
+      database.createRole(newRoleInfo).then(result => {
+        console.table(result);
+        init();
       });
-    });    
+    });
+  });
 };
 
-// addEmployee() 
+function addEmployee() {
 
-  init();
+  database.readRoles().then(roles => {
+    let roleOptions = roles.map(roles => ({
+      value: roles.role_id,
+      name: roles.role_title
+    }));
+
+    inquirer.prompt([
+      {
+       type: "list",
+        name: "roleId",
+        message: "What is the role of this employee?",
+        choices: roleOptions
+      },
+    ]).then(newEmployeeInfo => {
+      database.createEmployee(newEmployeeInfo).then(result => {
+        console.table(result);
+        init();
+      });
+    });
+  });
+};
+
+init();
